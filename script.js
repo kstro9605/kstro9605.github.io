f797fbe9d9c87089662f049b7fdc686be9a1ae43
kstrother <kstro9605@outlook.com>
2023-11-08 12:13:40 -0700

    Make it look better

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // script.js
 
 // Add any interactivity or functionality here
+document.addEventListener("DOMContentLoaded", function() {
+    const navPanes = document.querySelectorAll(".nav-pane");
+    const views = document.querySelectorAll(".view");
+
+    navPanes.forEach((pane, index) => {
+        pane.addEventListener("click", () => {
+            // Hide all views
+            views.forEach(view => (view.style.display = "none"));
+
+            // Show the selected view
+            views[index].style.display = "block";
+        });
+    });
+
+    // Show the first view by default
+    views[0].style.display = "block";
+});
