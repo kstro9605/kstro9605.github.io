109e21ff6d1dbc92fa546953d9308cebcce5ce3d
kstrother <kstro9605@outlook.com>
2023-11-08 13:14:02 -0700

    Overhaul

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// script.js
-
-// Add any interactivity or functionality here
-document.addEventListener("DOMContentLoaded", function() {
-    const navPanes = document.querySelectorAll(".nav-pane");
-    const views = document.querySelectorAll(".view");
-
-    navPanes.forEach((pane, index) => {
-        pane.addEventListener("click", () => {
-            // Hide all views
-            views.forEach(view => (view.style.display = "none"));
-
-            // Show the selected view
-            views[index].style.display = "block";
-        });
-    });
-
-    // Show the first view by default
-    views[0].style.display = "block";
-});
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  function showJob(id)
+  {
+    var content = document.getElementById(id);
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+  }
+  
+  
